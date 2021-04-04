3c50af8fc815478e5ec0a5957b8ddb3c60a72577
aya <ayaaa1027@gmail.com>
2021-04-04 16:01:46 +0900

    first commit

--- src/components/Othello.tsx
+++ src/components/Othello.tsx
@@ -1,7 +1,67 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Cell } from './Cell';
+import { CellStatus } from '../types';
+import { reverse } from '../utils/othelloUtil';
+
+const cells: Array<CellStatus> = Array<CellStatus>(8).fill(CellStatus.White);
+const getInitialRows = (): Array<Array<CellStatus>> => {
+  //一次元配列を生成
+  const row: Array<CellStatus> = Array<CellStatus>(8).fill(CellStatus.Empty);
+  const rows: Array<Array<CellStatus>> = [];
+  for (let index = 0; index < 8; index++) {
+    //生成した一次元配列のコピーを二次元配列に追加
+    const newRow = [...row];
+    rows.push(newRow);
+  }
+
+  rows[3][4] = CellStatus.Black;
+  rows[4][3] = CellStatus.Black;
+  rows[3][3] = CellStatus.White;
+  rows[4][4] = CellStatus.White;
+
+  return rows;
+};
+
 export const Othello = () => {
+  const [isBlackTurn, setIsBlackTurn] = useState(true);
+  const [rows, setRows] = useState(getInitialRows());
+
+  const onClickCell = () => {
+    const result = reverse();
+    if (result === null) return;
+
+    setRows(result);
+    setIsBlackTurn(!isBlackTurn);
+  };
+
   return (
     <>
-      <h3>Hello</h3>
+      <SContainer>
+        <div>
+          {isBlackTurn ? '黒の番' : '白の番'}
+          {rows.map((row, index) => {
+            return (
+              <SRow key={index}>
+                {row.map((status, index) => {
+                  return <Cell key={index} status={status} onClick={onClickCell} />;
+                })}
+              </SRow>
+            );
+          })}
+        </div>
+      </SContainer>
     </>
   );
 };
+
+const SRow = styled.div`
+  display: flex;
+`;
+
+const SContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/utils/othelloUtil.ts
@@ -0,0 +1,11 @@
+import { CellStatus } from '../types';
+import { getSample1 } from '../mock';
+
+export const reverse = (): Array<Array<CellStatus>> | null => {
+  const result = getSample1();
+  return result;
+};
+
+const getPiecesNum = () => {
+  return null;
+};
